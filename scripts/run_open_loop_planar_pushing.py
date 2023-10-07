4f36ba17d51d524fd7130dc21989071e639e9eb3
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-10-07 00:31:48 -0400

    Add script for open loop planar pushing

---
+++ scripts/run_open_loop_planar_pushing.py
@@ -0,0 +1,144 @@
+import argparse
+import logging
+
+import numpy as np
+
+from manipulation.station import load_scenario
+from pydrake.all import (
+    DiagramBuilder,
+    MeshcatVisualizer,
+    PiecewisePose,
+    RigidTransform,
+    RollPitchYaw,
+    Simulator,
+)
+
+from iiwa_setup.controllers import OpenLoopPlanarPushingController
+from iiwa_setup.iiwa import IiwaHardwareStationDiagram
+
+
+def main(
+    use_hardware: bool,
+    html_path: str,
+    pushing_start_pose: RigidTransform,
+    pushing_pose_trajectory: PiecewisePose,
+) -> None:
+    scenario_data = """
+    directives:
+    - add_directives:
+        file: package://iiwa_setup/iiwa7.dmd.yaml
+    plant_config:
+        # For some reason, this requires a small timestep
+        time_step: 0.0001
+        contact_model: "hydroelastic"
+        discrete_contact_solver: "sap"
+    model_drivers:
+        iiwa: !IiwaDriver {}
+    """
+
+    builder = DiagramBuilder()
+
+    scenario = load_scenario(data=scenario_data)
+    station: IiwaHardwareStationDiagram = builder.AddNamedSystem(
+        "station",
+        IiwaHardwareStationDiagram(
+            scenario=scenario, has_wsg=False, use_hardware=use_hardware
+        ),
+    )
+
+    controller_plant = station.internal_station.GetSubsystemByName(
+        "iiwa.controller"
+    ).get_multibody_plant_for_control()
+    controller: OpenLoopPlanarPushingController = builder.AddNamedSystem(
+        "open_loop_planar_pushing_controller",
+        OpenLoopPlanarPushingController(
+            controller_plant=controller_plant,
+            num_joint_positions=7,
+            pushing_start_pose=pushing_start_pose,
+            pushing_pose_trajectory=pushing_pose_trajectory,
+            gripper_frame_name="iiwa_link_7",
+            initial_delay_s=1.0,
+            wait_push_delay_s=1.0,
+        ),
+    )
+    builder.Connect(
+        station.GetOutputPort("iiwa.position_measured"),
+        controller.GetInputPort("iiwa.position_measured"),
+    )
+    builder.Connect(
+        station.GetOutputPort("iiwa.state_estimated"),
+        controller.GetInputPort("iiwa.state_estimated"),
+    )
+    builder.Connect(
+        controller.GetOutputPort("iiwa.position"),
+        station.GetInputPort("iiwa.position"),
+    )
+
+    visualizer = MeshcatVisualizer.AddToBuilder(
+        builder, station.GetOutputPort("query_object"), station.internal_meshcat
+    )
+
+    diagram = builder.Build()
+    simulator = Simulator(diagram)
+    simulator.set_target_realtime_rate(1.0)
+
+    visualizer.StartRecording()
+    station.internal_meshcat.AddButton("Stop Simulation")
+    while (
+        station.internal_meshcat.GetButtonClicks("Stop Simulation") < 1
+        and not controller.is_finished()
+    ):
+        simulator.AdvanceTo(simulator.get_context().get_time() + 0.1)
+    station.internal_meshcat.DeleteButton("Stop Simulation")
+    visualizer.StopRecording()
+    visualizer.PublishRecording()
+
+    if html_path is not None:
+        html = station.internal_meshcat.StaticHtml()
+        with open(html_path, "w") as f:
+            f.write(html)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--use_hardware",
+        type=bool,
+        default=False,
+        help="Whether to use real world hardware.",
+    )
+    parser.add_argument(
+        "--html_path",
+        type=str,
+        default=None,
+        help="The path to save the simulation recording Meshcat HTML to. No HTML is "
+        + "saved if no path is provided.",
+    )
+    parser.add_argument(
+        "--log_level",
+        type=str,
+        default="INFO",
+        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
+        help="Log level.",
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log_level)
+
+    # TODO: The path should be passes as an argument
+    poses = [
+        RigidTransform(RollPitchYaw(np.pi, 0.0, np.pi / 2), [0.0, -0.45, 0.3]),
+        RigidTransform(RollPitchYaw(np.pi, 0.0, np.pi / 2), [0.0, -0.6, 0.3]),
+        RigidTransform(RollPitchYaw(np.pi, 0.0, np.pi / 2), [0.0, -0.75, 0.3]),
+    ]
+    pushing_pose_traj = PiecewisePose.MakeLinear(
+        times=np.arange(len(poses)),
+        poses=poses,
+    )
+
+    main(
+        use_hardware=args.use_hardware,
+        html_path=args.html_path,
+        pushing_start_pose=poses[0],
+        pushing_pose_trajectory=pushing_pose_traj,
+    )
