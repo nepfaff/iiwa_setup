f6dc57af225a90b9518cdc0ca3bc8cf39f13ab79
nepfaff <nepfaff@mit.edu>
2024-03-08 16:14:40 -0500

    Check for package existance before adding

--- iiwa_setup/util/util.py
+++ iiwa_setup/util/util.py
@@ -5,6 +5,7 @@ from typing import List
 
 def get_package_xmls() -> List[str]:
     """Returns a list of package.xml files."""
-    return [
-        os.path.abspath("models/package.xml"),
-    ]
+    path = os.path.abspath("models/package.xml")
+    if os.path.exists(path):
+        return [path]
+    return []
